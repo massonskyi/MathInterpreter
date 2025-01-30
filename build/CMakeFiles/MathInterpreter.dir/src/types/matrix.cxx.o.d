1c8f46299c924ddc4558f175b1102f72d41da33b
Andrey <65669261+massonskyi@users.noreply.github.com>
2025-01-30 18:11:19 +0300

    Delete build directory

--- build/CMakeFiles/MathInterpreter.dir/src/types/matrix.cxx.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MathInterpreter.dir/src/types/matrix.cxx.o: \
- /home/massonskyi/repo/kp_toop/kp-toop-final/src/types/matrix.cxx \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/MathInterpreter.dir/src/types/variable.cxx.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MathInterpreter.dir/src/types/variable.cxx.o: \
- /home/massonskyi/repo/kp_toop/kp-toop-final/src/types/variable.cxx \
- /usr/include/stdc-predef.h
